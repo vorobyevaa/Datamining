370f8c210e87fc6dcb62fa15514538bf2f8c1248
o.kryuchin <o.kryuchin@platiza.ru>
2023-06-13 12:08:32 +0300

    парсим json

--- report.cpp
+++ report.cpp
@@ -1,6 +1,25 @@
 #include "report.h"
+#include <QJsonDocument>
+#include <QJsonArray>
+#include <QtDebug>
+#include <QJsonObject>
 
 Report::Report()
 {
+    m_content = "{'Model': {'Название': ['Первый', 'Второй', 'Третий', 'Четвертый']}}";
+    qDebug()<<m_content;
+    QJsonParseError error;
+    QJsonDocument jsonDocument = QJsonDocument::fromJson(m_content.toUtf8(), &error);
+
+    qDebug() << "Error: " << error.errorString() << error.offset << error.error;
+
+
+    qDebug()<<jsonDocument.isObject();
+    qDebug()<<jsonDocument.isArray();
+    qDebug()<<jsonDocument.isEmpty();
+    qDebug()<<jsonDocument.object().value("Model");
+    QJsonArray arrayData =  jsonDocument.array();
+qDebug()<<arrayData;
 
 }
+
--- report.h
+++ report.h
@@ -1,11 +1,14 @@
 #ifndef REPORT_H
 #define REPORT_H
 
+#include <QString>
 
 class Report
 {
 public:
     Report();
+private:
+    QString m_content;
 };
 
 #endif // REPORT_H
